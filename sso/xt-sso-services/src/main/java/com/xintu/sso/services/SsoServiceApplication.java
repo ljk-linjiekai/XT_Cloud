15e2512014b3de8888f7752e63cc36b072ae233e
linjiekai <13509030019@163.com>
2019-04-11 17:38:53 +0800

    依赖整理

--- sso/xt-sso-services/src/main/java/com/xintu/sso/services/SsoServiceApplication.java
+++ sso/xt-sso-services/src/main/java/com/xintu/sso/services/SsoServiceApplication.java
@@ -1,14 +1,9 @@
 package com.xintu.sso.services;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
