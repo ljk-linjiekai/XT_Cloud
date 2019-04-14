aff730ab50e8af372ebcea92349acbcdbaea137a
linjiekai <13509030019@163.com>
2019-04-14 17:36:43 +0800

    11

---
+++ xt-config-service/src/test/java/com/xintu/xtconfigservice/XtConfigServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xintu.xtconfigservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XtConfigServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
